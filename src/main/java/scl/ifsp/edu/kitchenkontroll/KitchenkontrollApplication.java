c5c0103ef2b1030517bb4a839afb3bbdba95a17b
Lygia Ferronato Bueno <lybueno@gmail.com>
2023-04-21 22:18:07 -0300

    0: Project created

---
+++ src/main/java/scl/ifsp/edu/kitchenkontroll/KitchenkontrollApplication.java
@@ -0,0 +1,13 @@
+package scl.ifsp.edu.kitchenkontroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenkontrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenkontrollApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scl/ifsp/edu/kitchenkontroll/KitchenkontrollApplicationTests.java
@@ -0,0 +1,13 @@
+package scl.ifsp.edu.kitchenkontroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenkontrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
